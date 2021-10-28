3e3158949f4d1ee8933145039479962de428156b
LTD <andreferi135@gmail.com>
2021-10-29 06:53:38 +0700

    fixing ios dynamic issue

--- src/screens/LoadingScreen.tsx
+++ src/screens/LoadingScreen.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {View, ActivityIndicator} from 'react-native';
+import {View, ActivityIndicator, Linking} from 'react-native';
 import {Colors} from '../assets/themes';
 import CText from '../components/CText';
 import AsyncStorage from '@react-native-community/async-storage';
@@ -8,6 +8,7 @@ import dynamicLinks from '@react-native-firebase/dynamic-links';
 import GlobalStyles from '../public/styles/GlobalStyles';
 import NavigationServices from '../routes/NavigationServices';
 import {userServices} from '../public/services';
+import {iOS} from '../public/helper/GlobalHelper';
 
 const LoadingScreen = () => {
   useEffect(() => {
@@ -15,20 +16,46 @@ const LoadingScreen = () => {
   }, []);
 
   const fetchInitialize = async () => {
+    let source: any;
+
     const jwt = await AsyncStorage.getItem('BEARER_TOKEN');
 
     const dynamicLink = await dynamicLinks().getInitialLink();
 
-    if (dynamicLink?.url) {
-      if (dynamicLink.utmParameters?.utm_campaign === 'change-password') {
-        if (dynamicLink.utmParameters?.utm_source) {
-          const source = dynamicLink.utmParameters.utm_source.split('&');
-          console.log(source);
+    if (iOS && !dynamicLink?.url) {
+      const url = await Linking.getInitialURL();
+      const screenIndex = url?.indexOf('utm_campaign=change-password');
+
+      if (screenIndex) {
+        if (screenIndex > -1) {
+          const txt = 'utm_source=';
+          const sourceIndex = url?.lastIndexOf('utm_source=') as number;
+          const sourceRes = url?.substr(sourceIndex);
+          const cidIndex = sourceRes?.indexOf('&cid') as number;
+
+          const utmSource = url?.substr(
+            sourceIndex + txt.length,
+            cidIndex - txt.length,
+          );
+
+          source = utmSource?.split('%26');
 
           return NavigationServices.navigate('ChangePassword', {
             email: source[0],
             code: source[1],
           });
+        }
+      }
+    } else {
+      if (dynamicLink?.url) {
+        if (dynamicLink.utmParameters?.utm_campaign === 'change-password') {
+          if (dynamicLink.utmParameters?.utm_source) {
+            source = dynamicLink.utmParameters.utm_source.split('&');
+            return NavigationServices.navigate('ChangePassword', {
+              email: source[0],
+              code: source[1],
+            });
+          }
         }
       }
     }
